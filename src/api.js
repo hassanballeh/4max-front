a158cf2a4b55dfa083c172b69f28e4c25a9bbbc4
Hasan Balleh <hassanballeh@gmail.com>
2025-09-06 19:09:00 +0300

    add railway again1

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https:clothes-back-production.up.railway.app/api", // Change to your backend
+  baseURL: "https://clothes-back-production.up.railway.app/api", // Change to your backend
   // withCredentials: true, // optional if using cookies
   headers: {
     "Content-Type": "application/json",
